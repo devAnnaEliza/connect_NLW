cadf5a4bc510f13cc47a46bf0b655d96cab1a29b
Anna Eliza <annaeliza.als@gmail.com>
2025-02-22 16:59:33 -0300

    .

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const getTotalSubscribers = (userData) => {
     const subs = users.filter((user) => {
         return user.refBy == userData.ref
     })
+    return subs.length
 }
 
 const showInvite = (userData) => {
@@ -34,7 +35,7 @@ const showInvite = (userData) => {
 
     <div id="stats">
         <h4>
-            80
+            ${getTotalSubscribers(userData)}
         </h4>
         <p>
             Inscrições feitas
@@ -43,18 +44,35 @@ const showInvite = (userData) => {
     `
 }
 
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() *4000),
+        refBy: 100
+    }
+
+    users.push(newUser)
+    console.log(users)
+    return newUser
+}
+
 const formAction = () => {
     const form = document.getElementById('form')
     form.onsubmit = (event) => {
         event.preventDefault()
         const formData = new FormData(form)
         const userData = {
-            email: formData.get('email')
-            phone: formData.get('phone')
+            email: formData.get('email'),
+            phone: formData.get('phone'),
         }
 
         const user = getUser(userData)
-        console.log(user)
+        if(user) {
+            showInvite(user)
+        } else {
+            const newUser = saveUser(userData)
+            showInvite(newUser)
+        }
 
     }
 }
@@ -75,3 +93,5 @@ const startApp = () => {
     formAction()
 }
 startApp()
+
+document.getElementById("logo").onclick = () => startApp()
