6c78e17c9a927b52a566ae5ce83f369a61db1d1b
Anna Eliza <annaeliza.als@gmail.com>
2025-02-22 19:56:16 -0300

    last commit

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-## ???? 
+## Connect 
 **`RocketSeat`** 🚀
 
 Este projeto irá desenvolver um software de inscrição e indicação para eventos com diversas funcionalidades. Nele, o usuário pode acessar seu ingresso, gerar seu link de convite e ter acesso ao ranking de indicações. Foi desenvolvido durante o envento NLW Connect da Rocketseat.
-
-
-
-\\ att em andamento \\
--- script.js
+++ script.js
@@ -31,17 +31,34 @@ const getTotalSubscribers = (userData) => {
 
 const showInvite = (userData) => {
     app.innerHTML = `
-    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+      <main>
+        <h3>Inscrição confirmada!</h3>
 
-    <div id="stats">
+        <p>
+          Convide mais pessoas e concorra a prêmios! </br>
+          Compartilhe o link e acompanhe as inscrições:
+        </p>
+
+        <div class="input-group">
+          <label for="link">
+            <img src="link.svg" alt="link icon">
+          </label>
+          <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+
+        </div>
+      </main>
+
+      <section class="stats">
         <h4>
-            ${getTotalSubscribers(userData)}
+          ${getTotalSubscribers(userData)}
         </h4>
         <p>
-            Inscrições feitas
+          Inscrições feitas
         </p>
-    </div>
-    `
+      </section>
+  `
+  app.setAttribute('class', 'page-invite')
+  updateImageLinks()
 }
 
 const saveUser = (userData) => {
@@ -77,21 +94,74 @@ const formAction = () => {
     }
 }
 
+const updateImageLinks = () => {
+  document.querySelectorAll('img').forEach((img) => {
+    if(img.src.includes('githubusercontent')){
+      return
+    }
+    const src = img.getAttribute('src')
+    img.src = `https://raw.githubusercontent.com/maykbrito/my-public-files/main/nlw-19/${src}`
+  })
+}
+
 const startApp = () => {
     const content = `
-    <form id="form">
-        <input type="email" name="email" placeholder="E-mail" />
-        <input type="text" name="phone" placeholder="Telefone" />
-         <button>
+    <main>
+      <section class="about">
+        <div class="section-header">
+          <h2>
+            Sobre o evento
+          </h2>
+          <span class="badge">Ao Vivo</span>
+        </div>
+
+        <p>
+          Um evento feito por e para pessoas desenvolvedoras apaixonadas por criar soluções inovadoras e compartilhar conhecimento. Vamos mergulhar nas tendências mais recentes em desenvolvimento de software, arquitetura de sistemas e tecnologias emergentes, com palestras, workshops e hackathons.
+          <br></br> Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito 
+        </p>
+      </section>
+
+      <section class="registration">
+        <h2>Inscrição</h2>
+
+        <form id="form">
+          <div class="input-wrapper">
+            <div class="input-group">
+              <label for="email">
+                <img src="mail.svg" alt="email icon">
+              </label>
+              <input type="email" id="email" name="email" placeholder="E-mail">
+            </div>
+
+            <div class="input-group">
+              <label for="phone">
+                <img src="phone.svg" alt="phone icon">
+              </label>
+              <input type="text" id="phone" name="phone" placeholder="Telefone">
+            </div>
+          </div>
+
+          <button>
             Confirmar
-         </button>
-    </form>
-    `
+            <img src="arrow.svg" alt="arrow right">
+          </button>
+        </form>
+      </section>
+    </main>
+  `
 
     app.innerHTML = content
-
+    app.setAttribute('class', 'page-start')
+    updateImageLinks()
     formAction()
 }
+
 startApp()
 
-document.getElementById("logo").onclick = () => startApp()
+//showInvite({
+  //email: 'test@test.com',
+  //phone: '9999',
+  //ref: 100
+//})
+
+document.querySelector("header").onclick = () => startApp()
